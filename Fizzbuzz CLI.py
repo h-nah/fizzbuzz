1c779abf717b0941725f06d2452841db7a128017
Hanah Kim <hanah@raspi.com>
2022-07-22 13:44:49 +0200

    Fizzbuzz  works

--- Fizzbuzz CLI.py
+++ Fizzbuzz CLI.py
@@ -1,9 +1,12 @@
 for Zahl in range(1,60,1):
-    if Zahl%3 == 0:
-        print("Fizz")
-    if Zahl%5 == 0:
-        print("Buzz")
     if Zahl%5 == 0 and Zahl%3 == 0:
         print("Fizzbuzz")
+    elif  Zahl%5 == 0:
+        print("Buzz")
+    elif  Zahl%3 == 0:
+        print("Fizz")
     else:
         print(Zahl)
+
+
+    
